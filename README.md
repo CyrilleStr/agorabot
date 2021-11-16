1fc00d1b33790b094ef5cd77f14f108109c14ff6
Cyrille Stroesser <cyrille.stroesser@orange.fr>
2021-11-16 11:36:50 +0100

    add readme and show resonses on page

---
+++ README.md
@@ -0,0 +1,8 @@
+# Agora-bot
+## About
+Firefox extensions showing good answers of agora-quiz's quiz
+## Installation
+- <a href="about:debugging#/runtime/this-firefox">Click here</a>
+- Then click on the "Load Temporary Add-on..." button
+- Then select a file (```agorabot.js``` or ```manifest.json```)
+- It's ready
--- agorabot.js
+++ agorabot.js
@@ -1,61 +1,46 @@
-console.log("zebi activé")
-document.addEventListener("DOMContentLoaded", function () {
+console.log("Agorabot activé")
 
+let logo = document.createElement("div")
+logo.innerHTML = "Cheatcode activated"
+logo.setAttribute("style", "color: black;background: green;margin: 0.3em;padding: 0.3em;border-radius: 2em; margin-left:2em;")
+document.querySelector(".centered-button").appendChild(logo)
 
-    let logo = document.createElement("div")
-    logo.innerHTML = "Cheatcode activated"
-    logo.setAttribute("style", "color: black;background: green;margin: 0.3em;padding: 0.3em;border-radius: 2em;")
-    // document.querySelector(".centered-button").appendChild(logo)
+function filterData(details) {
+    let filter = browser.webRequest.filterResponseData(details.requestId);
+    let decoder = new TextDecoder("utf-8");
+    let encoder = new TextEncoder();
 
-    function filterData(details) {
-        let filter = browser.webRequest.filterResponseData(details.requestId);
-        let decoder = new TextDecoder("utf-8");
-        let encoder = new TextEncoder();
-
-        filter.ondata = event => {
-            let str = decoder.decode(event.data, { stream: true });
-            let body = JSON.parse(str)
-            filter.write(encoder.encode(str))
-            filter.disconnect()
-            let newDiv = document.createElement("div")
-            newDiv.innerHTML = "Question 1 = " + body.question_1.good_rep +
-                "\nQuestion 2 = " + body.question_2.good_rep +
-                "\nQuestion 3 = " + body.question_3.good_rep;
-            newDiv.setAttribute("style", "background-color: #e8fde1;padding: 0.5em;margin: 0.2em;border-radius: 1em;")
-            console.log("Question 1 = " + body.question_1.good_rep +
-                "\nQuestion 2 = " + body.question_2.good_rep +
-                "\nQuestion 3 = " + body.question_3.good_rep)
-            // document.query("users-and-round")[0].appendChild(newDiv)
-        }
+    filter.ondata = event => {
+        // Get reqeuest data and release the request
+        let str = decoder.decode(event.data, { stream: true });
+        let body = JSON.parse(str)
+        filter.write(encoder.encode(str))
+        filter.disconnect()
+        // Create the response div
+        let responses = "Question 1 = " + body.question_1.good_rep +
+            "\nQuestion 2 = " + body.question_2.good_rep +
+            "\nQuestion 3 = " + body.question_3.good_rep;
+        console.log(responses)
+        let newDiv = document.createElement("div")
+        newDiv.innerHTML = responses;
+        newDiv.setAttribute("style", "background-color: #e8fde1;padding: 0.5em;margin: 0.2em;border-radius: 1em;")
+        document.query("users-and-round")[0].appendChild(newDiv)
     }
-
-    browser.webRequest.onBeforeRequest.addListener(
-        function logURL(details) {
-            if ((details.method == "POST" && details.url.includes("new_round"))) {
-                console.log("new_round")
-                filterData(details)
-            }
-            if (details.method == "GET" && details.url.includes("get_last_round")) {
-                console.log("get_last_round")
-                filterData(details)
-            }
-            return {}
-        },
-        { urls: ["https://*.agora-quiz.education/*"] },
-        ["blocking"]
-    );
-})
-
-/*
-"content_scripts": [
-        {
-            "matches": [
-                "https://*.agora-quiz.education/*"
-            ],
-            "js": [
-                "agorabot.js"
-            ]
-        }
-    ]
 }
-*/
+
+browser.webRequest.onBeforeRequest.addListener(
+    function logURL(details) {
+        console.log("Request listened")
+        if ((details.method == "POST" && details.url.includes("new_round"))) {
+            console.log("new_round")
+            filterData(details)
+        }
+        if (details.method == "GET" && details.url.includes("get_last_round")) {
+            console.log("get_last_round")
+            filterData(details)
+        }
+        return {}
+    },
+    { urls: ["https://*.agora-quiz.education/*"] },
+    ["blocking"]
+);
