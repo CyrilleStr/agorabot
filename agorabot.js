9d196e4ee6df01962f8e48370d4fb4e7df1e76a2
Cyrille Stroesser <cyrille.stroesser@orange.fr>
2021-11-17 16:05:13 +0100

    suppression de la div avant d'afficher la nouvelle

--- agorabot.js
+++ agorabot.js
@@ -8,13 +8,16 @@ document.querySelector(".centered-button").appendChild(logo)
 
 // Wait for the background-script to send responeses
 browser.runtime.onMessage.addListener(request => {
-    console.log("Réponses :")
-    console.log(request.rep1 + request.rep2 + request.rep3)
+    console.log("Réponses :\n")
+    console.log(request.rep1 + "\n" + request.rep2 + "\n" + request.rep3)
+    let oldDiv = document.getElementById("agorabot-responses")
+    if (oldDiv) oldDiv.remove();
     let newDiv = document.createElement("div")
     newDiv.innerHTML = "Question 1 = " + request.rep1 + "<br>" +
         "Question 2 = " + request.rep2 + "<br>" +
         "Question 3 = " + request.rep3 + "<br>";
     newDiv.setAttribute("style", "background-color: #e8fde1;padding: 0.5em;margin: 1em;border-radius: 1em;")
+    newDiv.setAttribute("id", "agorabot-responses")
     document.getElementsByClassName("users-and-round")[0].appendChild(newDiv)
 });
 console.log("Agorabot activé")
